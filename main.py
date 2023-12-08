1c0191b5af97a616f06d6e6528648255dc1c6e67
John <john@Johns-MBP.lan>
2023-12-08 13:42:04 -0800

    startproject and startapp

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 # This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Start Project to create core admin app
+# Step 1
+# django-admin startproject crm
+# Step 2
+# python3 manage.py runserver
+# Step 3
+# create the app django-admin startapp webapp
+# Step 4
+# go to settings.py and add the app you created
+# Step 5
+# migrate the app python3 manage.py migrate
